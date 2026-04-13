eeeeae1c94c0bbff0760094b6959ca19de29cc41
Garvit Goyal <garvitgoyal29557@gmail.com>
2026-04-14 00:44:24 +0530

    change mongo query in getMeal

--- food-backend/controller/getMeal.js
+++ food-backend/controller/getMeal.js
@@ -1,13 +1,15 @@
 import donateModel from '../model/donateModel.js'
 const getMeal = async (req, res) => {
     try {
-        const dataForListingMeal = await donateModel.find({status: "accepted" })
+        const dataForListingMeal = await donateModel.find({
+            status: { $in: ["accepted", "completed"] }
+        })
         const len = dataForListingMeal.length;
         console.log(len)
         res.status(200).json({
             success: true,
             message: "Donation meal list successfully",
-            data:len
+            data: len
         })
     } catch (err) {
         console.log(err);
